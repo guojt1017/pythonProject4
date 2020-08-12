ca4d8fc24f3521b22a1ac0dd8ebf7f9f8e9ba004
guojt1017 <guojt1997@sina.com>
2020-08-12 18:48:37 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+if __name__ == '__main__':
+    import numpy as np
+    a=[1,2,3,4]
+    b=np.array(a)
+    print(b)
